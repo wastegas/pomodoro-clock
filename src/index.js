c5945140db401cfc35b2a8e5160257d27172982e
wastegas <wastegas@gmail.com>
2017-05-30 21:40:05 -1000

    add logic for decreasing and increasing timer values

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import './style.scss';
 
 $(document).ready(function() {
+	$('button').click(function() {
+		var curVal;
+		var newVal;
+		switch ($('button').index(this)) {
+			case 0:
+				curVal = $('span:first').text();
+				newVal = parseInt(curVal) - 1;
+				if (newVal < 0) { 
+					break;
+				}
+				$('span:first').html(newVal);
+				break;
+			case 1:
+				curVal = $('span:first').text();
+				newVal = parseInt(curVal) + 1;
+				$('span:first').html(newVal);
+				break;
+			case 2:
+				curVal = $('span:last').text();
+				newVal = parseInt(curVal) - 1;
+				if (newVal < 0) {
+					break;
+				}
+				$('span:last').html(newVal);
+				break;
+			case 3:
+				curVal = $('span:last').text();
+				newVal = parseInt(curVal) + 1;
+				$('span:last').html(newVal);
+				break;
+		}
+	})
+
 	$('#timer').click(function() {
 		$('.inner').slideDown('slow')
 	})
