b2310d481d0d523f5720ffcc6b0a333f904bd470
wastegas <wastegas@gmail.com>
2017-06-03 22:21:43 -1000

    new styles

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import moment from 'moment';
+import 'font-awesome-webpack';
 import './style.scss';
 
 $(document).ready(function() {
 	$('button').click(function() {
 		let curVal;
 		let  newVal;
+		console.log($('button').index(this));
 		switch ($('button').index(this)) {
-			case 0:
-				curVal = $('span:first').text();
+			case 1:
+				curVal = $('#brkTime').text();
 				newVal = parseInt(curVal) - 1;
 				if (newVal < 0) { 
 					break;
 				}
-				$('span:first').html(newVal);
-				break;
-			case 1:
-				curVal = $('span:first').text();
-				newVal = parseInt(curVal) + 1;
-				$('span:first').html(newVal);
+				$('#brkTime').html(newVal);
 				break;
 			case 2:
-				curVal = $('span:last').text();
+				curVal = $('#brkTime').text();
+				newVal = parseInt(curVal) + 1;
+				$('#brkTime').html(newVal);
+				break;
+			case 3:
+				curVal = $('#sessTime').text();
 				newVal = parseInt(curVal) - 1;
 				if (newVal < 0) {
 					break;
 				}
-				$('span:last').html(newVal);
+				$('#sessTime').html(newVal);
 				$('.content').html(newVal);
 				break;
-			case 3:
-				curVal = $('span:last').text();
+			case 4:
+				curVal = $('#sessTime').text();
 				newVal = parseInt(curVal) + 1;
-				$('span:last').html(newVal);
+				$('#sessTime').html(newVal);
 				$('.content').html(newVal);
 				break;
 		}
@@ -43,17 +45,17 @@ $(document).ready(function() {
 	$('#timer').click(function() {
 		running = !running;
 		function loop() {
-			let duration = parseInt($('span:last').text());
-			let workDur = duration * 60000;
-			duration = parseInt($('span:first').text());
-			let breakDur = duration * 60000;
-			updateContent(workDur);
+			let duration = parseInt($('#sessTime').text());
+			const workDur = duration * 60000;
+			duration = parseInt($('#brkTime').text());
+			const breakDur = duration * 60000;
+			updateContent(workDur, 'SESSION');
 			$('.inner')
 			.queue(function() {
 				$(this).toggleClass('newColor').dequeue();
 			})
 			.animate({height: "100%"}, workDur, 'linear', function() {
-					updateContent(breakDur)}) 
+					updateContent(breakDur, 'BREAK')}) 
 			.queue(function() {
 				$(this).toggleClass('newColor').dequeue();
 			}) 
@@ -69,8 +71,9 @@ $(document).ready(function() {
 			$('.inner').attr('style','');
 		}
 	})
-	function updateContent(duration) {
+	function updateContent(duration, curSession) {
 		let  content = duration / 1000;
+		$('.session').html(curSession);
 		intervalId = setInterval(function() {
 			const strTime = moment('1900-01-01 00:00:00')
 							.add(--content, 'seconds')
